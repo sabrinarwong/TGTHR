fde515d2472d85faa270f8bde4616b60780677a3
Ryan Yuzuki <ryuzu001@ucr.edu>
2018-10-25 18:19:36 -0700

    homepage working on

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # TGTHR
 TGTHR brings people together in thousands of cities to do more of what they want to do in life. It is organized around one simple idea: when we get together and do the things that matter to us, we’re at our best. And that’s what TGTHR does. It helps people meet up, explore, teach and learn the things that help them come alive.
+
+
+
+
+# running
+
+to run this, cd into src/TGTHR and run the command `expo start`
+
+make sure to have an android device emulator running or connected through ADB.
+
+also `npm start --reset-cache` is helpful, especially if you run into problems (Directory exists but tells you it doesnt..etc)
--- src/TGTHR/screens/HomeScreen.js
+++ src/TGTHR/screens/HomeScreen.js
@@ -7,6 +7,8 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Animated,
+  Button,
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
@@ -17,48 +19,82 @@ export default class HomeScreen extends React.Component {
     header: null,
   };
 
+  constructor(){
+    super();
+    this.animated = new Animated.Value(0);
+  }
+
+  animate(){
+    Animated.timing(this.animated,{
+      toValue:1,
+      duration:3000,
+    }).start();
+  }
+
   render() {
+    const opacity = this.animated.interpolate({
+      inputRange:[0,1],
+      outputRange:[0,1]
+    });
     return (
+
+
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <View style={styles.welcomeContainer}>
             <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
+              source={require('../assets/images/robot-dev.png')}
               style={styles.welcomeImage}
             />
+            {/* for some reason, comments are like his so idk */}
+            {/* displays the tgthr logo with the 'welcomeImage' style specified below. */}
           </View>
 
           <View style={styles.getStartedContainer}>
+
+            {/*
             {this._maybeRenderDevelopmentModeWarning()}
+            */}
 
-            <Text style={styles.getStartedText}>Get started by opening</Text>
+            {this.animate()}
 
+            <Text style={styles.getStartedText}>Welcome to </Text>
+            <Animated.Text style={[styles.tgthrMain, {backgroundColor: 'transparent'},{opacity}]}>
+              TGTHR
+            </Animated.Text>
+
+            {/*
             <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
               <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
             </View>
-
+            */}
             <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
+              The better way to meet.
             </Text>
           </View>
 
+          <View style={styles.buttonMain}>
+          <Button title="I need help!" onPress={()=>this.animate()}/>
+
+          </View>
+          {/*
           <View style={styles.helpContainer}>
             <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
               <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
             </TouchableOpacity>
           </View>
+          */}
         </ScrollView>
 
         <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
+          <Text style={styles.tabBarInfoText}>
+            Disclaimer, the button does nothing at the moment.
+          </Text>
+          {/*
           <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
             <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
           </View>
+          */}
         </View>
       </View>
     );
@@ -119,9 +155,9 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   welcomeImage: {
-    width: 100,
+    width: 150,
     height: 80,
-    resizeMode: 'contain',
+    resizeMode: 'center',
     marginTop: 3,
     marginLeft: -10,
   },
@@ -140,6 +176,17 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     paddingHorizontal: 4,
   },
+  buttonMain: {
+    marginTop: 50,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  tgthrMain: {
+    fontSize: 35,
+    color: '#9E5EE8',
+    lineHeight:50,
+    textAlign: 'center',
+  },
   getStartedText: {
     fontSize: 17,
     color: 'rgba(96,100,109, 1)',
@@ -151,17 +198,7 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
+    elevation: 20,
     alignItems: 'center',
     backgroundColor: '#fbfbfb',
     paddingVertical: 20,
@@ -170,6 +207,8 @@ const styles = StyleSheet.create({
     fontSize: 17,
     color: 'rgba(96,100,109, 1)',
     textAlign: 'center',
+    marginLeft:30,
+    marginRight:30,
   },
   navigationFilename: {
     marginTop: 5,
