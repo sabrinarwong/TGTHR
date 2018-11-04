1c9e10cf6e152ef213b831c093f9534316fc342b
Ryan Yuzuki <ryuzu001@ucr.edu>
2018-11-04 15:51:29 -0800

    firebase initalization, apikey (in gitignore)

--- src/TGTHR/App.js
+++ src/TGTHR/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import ApiKeys from './constants/ApiKeys';
+import * as firebase from 'firebase';
 
 export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoadingComplete: false,
+    };
+
+    // initialize firebase
+    if(!firebase.apps.length) {
+      firebase.initializeApp(ApiKeys.FirebaseConfig);
+    }  // prevents from loading twice in case of crash
+  }
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
